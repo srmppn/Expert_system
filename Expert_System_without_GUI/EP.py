c5fb37d5730dae4871dae0dc6787b1e43be4a3f1
srmppn <43701085+srmppn@users.noreply.github.com>
2018-11-10 20:50:19 +0700

    Update EP.py

--- Expert_System_without_GUI/EP.py
+++ Expert_System_without_GUI/EP.py
@@ -18,19 +18,15 @@ class get_rules:
         print("#-----------------RULES--------------------#")
         for i in range(self.numb_r):
             self.rules.append(input("# Enter rules: "))
-            if "and" in self.rules[i] or "or" in self.rules[i]:
-                # ---------------------------------------------
-                # this just split a phrase into several phrases
-                phrase = self.remov_it(self.rules[i]).split()
-                pre = phrase[1].split('-')
-                con = phrase[3].split('-')
-                for n in pre:
-                    for m in con:
-                        self.rules_ipm += ["if " + n + " then " + m]
-                        self.truth[n] = False
-            else:
-                self.rules_ipm += [ self.rules[i] ]
-                self.truth[self.rules[i][3]] = False
+            # ---------------------------------------------
+            # this just split a phrase into several phrases
+            phrase = self.remov_it(self.rules[i][3:]).split(' then ')
+            pre = phrase[0].split('-')
+            con = phrase[1].split('-')
+            for n in pre:
+                for m in con:
+                    self.rules_ipm += ["if " + n + " then " + m]
+                    self.truth[n] = False
     # ------------------------------------------------------
     # nothing just remove unnessary things that I don't want
     def remov_it(self, msg):
@@ -60,13 +56,13 @@ class kb:
     # successfully estabish
     def estabish_link(self):
         for r in self.rules_ipm:
-            conclusion = r.split()[3]
+            conclusion = r[3:].split(' then ')[1]
+            link = r[3:].split(' then ')[0]
             for j in self.rules_ipm:
                 if j is not r:
                     #premise = ['q','z']
-                    premise = j.split()[1]
+                    premise = j[3:].split(' then ')[0]
                     #link = ["x and y",'q']
-                    link = r.split()[1]
                     if premise in conclusion:
                         try:
                             self.linkage[ premise ] += [ link ]
@@ -76,20 +72,19 @@ class kb:
             else:
                 if conclusion not in self.linkage:
                     try:
-                        self.result[ conclusion ] += [ r.split()[1] ]
+                        self.result[ conclusion ] += [ link ]
                         self.result[ conclusion ] = list(set(self.result[ conclusion ]))
                     except KeyError:
-                        self.result[ conclusion ] = [ r.split()[1] ]
+                        self.result[ conclusion ] = [ link ]
         # --------------------------------------
-        # unnessary line just checking the result
-        '''  
+        # unnessary line just checking the result if it works properly
+        '''
+        print(self.rules_ipm)
         print(self.linkage)
         print(self.truth)
         print(self.result)
         '''
-
-    def remov_it(self, msg):
-        return msg.replace(' and ','-').replace(' or ','-')
+        #---------------------------------------
     def run(self):
         print("#---------------DIAGNOSING-----------------#")
         conclude = "Result: "
@@ -100,20 +95,21 @@ class kb:
         # Get the input from user if and only if it's the intermediate node
         prevent = []
         for R in self.rules_ipm:
-            prev = R[3]
+            prev = R[3:].split(' then ')[0]
             if prev not in self.linkage and prev not in prevent :
-                msg = "# Is " + prev + " True or False? (t/f) : "
+                msg = "# Is '" + prev + "' True or False? (t/f) : "
                 self.truth[prev] = input(msg) == "t"
                 prevent.append(prev)
         #-------------------------------------------------------------
         # Traverse into the trees and check all the values in there
         for K in self.rulse:
-            phrase = self.remov_it(K).split()
-            temp = phrase[3].split('-')
+            # msg.replace('if ','').replace(' then ',' ')
+            phrase = K.split(' then ')
+            temp = phrase[1].replace(' and ','-').replace(' or ','-').split('-')
             for check in temp:
                 # ----------------------------------------------------
                 # and-type checker
-                if "and" in K:
+                if "and" in phrase[0]:
                     if check in self.result:
                         count = 0
                         for re in self.result[check]:
@@ -128,12 +124,12 @@ class kb:
                             if self.truth[re]:
                                 count += 1
                         if count == len(self.linkage[check]) and check not in prevent:
-                            msg = "# Is " + check + " True or False? (t/f) : "
+                            msg = "# Is '" + check + "' True or False? (t/f) : "
                             self.truth[check] = input(msg) == "t"
                             prevent.append(check)
                 # ----------------------------------------------------
                 # or-type checker
-                elif "or" in K:
+                elif "or" in phrase[0]:
                     if check in self.result:
                         count = 0
                         for re in self.result[check]:
@@ -148,7 +144,7 @@ class kb:
                             if self.truth[re]:
                                 count += 1
                         if count > 0 and check not in prevent:
-                            msg = "# Is " + check + " True or False? (t/f) : "
+                            msg = "# Is '" + check + "' True or False? (t/f) : "
                             self.truth[check] = input(msg) == "t"
                             prevent.append(check)
                 # ----------------------------------------------------
@@ -168,7 +164,7 @@ class kb:
                             if self.truth[re]:
                                 count += 1
                         if count == len(self.linkage[check]) and check not in prevent:
-                            msg = "# Is " + check + " True or False? (t/f) : "
+                            msg = "# Is '" + check + "' True or False? (t/f) : "
                             self.truth[check] = input(msg) == "t"
                             prevent.append(check)
         # ----------------------------------------------------
